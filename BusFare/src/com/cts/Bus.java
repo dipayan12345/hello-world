ca178724b422e6d60aeacd0c5f92a93e753520eb
dipayan12345 <dipayan.dasgupta@cognizant.com>
2016-06-13 13:26:34 +0530

    committed by dipayan

---
+++ BusFare/src/com/cts/Bus.java
@@ -0,0 +1,32 @@
+package com.cts;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Bus {
+	
+	
+	public static void main(String args[]){
+		
+		String input_date="17/05/2016";
+		  SimpleDateFormat format1=new SimpleDateFormat("dd/MM/yyyy");
+		  Date dt1 = null;
+		try {
+			dt1 = format1.parse(input_date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  DateFormat format2=new SimpleDateFormat("EEEE"); 
+		  String finalDay=format2.format(dt1);
+		System.out.println(finalDay);
+		
+		
+		
+		
+	}
+	
+
+}
